0140993906a6cc5d28e073cbbf146000d27cd8a9
Do Minh Toan <minhtoando0899@gmail.com>
2020-04-02 01:29:52 +0700

    baitap

--- Bai7.py
+++ Bai7.py
@@ -12,10 +12,14 @@
 class Delete:
     def __init__(self, list):
         self.list = list
-        self.set_1 = {}
+        self.set_1 = set()
 
     def Set(self):
         for x in self.list:
             if x not in self.set_1:
-                set.add(self.set_1,x)
+                self.set_1.add(x)
 
+
+D1 = Delete([10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40])
+D1.Set()
+print(D1.set_1)
